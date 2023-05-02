871f0369ce0f140c7cf6ca64a47fd81bbef65ab5
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2023-05-02 19:12:36 -0400

    Moved several repositories back to master.

--- setup.sh
+++ setup.sh
@@ -23,148 +23,176 @@ script_directory=`pwd`
 
 
 # Installing ThirParty-HSL
-coinhsl_filename="coinhsl-2022.11.09"
-mkdir -p lib/ThirdParty-HSL/build
-cp lib/Sources-HSL/"${coinhsl_filename}.tar.gz"  lib/ThirdParty-HSL/"${coinhsl_filename}.tar.gz"
-cd lib/ThirdParty-HSL
-gunzip "${coinhsl_filename}.tar.gz"
-tar xf "${coinhsl_filename}.tar"
-ln -sf "${coinhsl_filename}" coinhsl
-cd build
-../configure
-make -j $njobs
-sudo make install
-cd "$script_directory"
+if [ ! -d lib/ThirdParty-HSL/build ]; then
+    coinhsl_filename="coinhsl-2022.11.09"
+    mkdir -p lib/ThirdParty-HSL/build
+    cp lib/Sources-HSL/"${coinhsl_filename}.tar.gz"  lib/ThirdParty-HSL/"${coinhsl_filename}.tar.gz"
+    cd lib/ThirdParty-HSL
+    gunzip "${coinhsl_filename}.tar.gz"
+    tar xf "${coinhsl_filename}.tar"
+    ln -sf "${coinhsl_filename}" coinhsl
+    cd build
+    ../configure
+    make -j $njobs
+    sudo make install
+    cd "$script_directory"
+fi
 
 
 # Installing MUMPS
-mkdir -p lib/ThirdParty-Mumps/build
-cd lib/ThirdParty-Mumps
-./get.Mumps
-cd build
-../configure 
-make -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/ThirdParty-Mumps/build ]; then
+    mkdir -p lib/ThirdParty-Mumps/build
+    cd lib/ThirdParty-Mumps
+    ./get.Mumps
+    cd build
+    ../configure 
+    make -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing Metis
-mkdir -p lib/ThirdParty-Metis/build
-cd lib/ThirdParty-Metis
-./get.Metis
-cd build
-../configure 
-make -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/ThirdParty-Metis/build ]; then
+    mkdir -p lib/ThirdParty-Metis/build
+    cd lib/ThirdParty-Metis
+    ./get.Metis
+    cd build
+    ../configure 
+    make -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing ASL
-mkdir -p lib/ThirdParty-ASL/build
-cd lib/ThirdParty-ASL
-./get.ASL
-cd build
-../configure
-make -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/ThirdParty-ASL/build ]; then
+    mkdir -p lib/ThirdParty-ASL/build
+    cd lib/ThirdParty-ASL
+    ./get.ASL
+    cd build
+    ../configure
+    make -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing IPOPT
-mkdir -p lib/Ipopt/build
-cd lib/Ipopt/build
-export ADD_CXXFLAGS="-std=c++17"
-../configure 
-make -j $njobs
-make test -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/Ipopt/build ]; then
+    mkdir -p lib/Ipopt/build
+    cd lib/Ipopt/build
+    export ADD_CXXFLAGS="-std=c++17"
+    ../configure 
+    make -j $njobs
+    make test -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing CPPAD
-mkdir -p lib/CppAD/build
-cd lib/CppAD/build
-cmake -D include_ipopt=true -D cppad_cxx_flags="-std=c++17" -D CMAKE_VERBOSE_MAKEFILE=YES ..
-make check -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/CppAD/build ]; then
+    mkdir -p lib/CppAD/build
+    cd lib/CppAD/build
+    cmake -D include_ipopt=true -D cppad_cxx_flags="-std=c++17" -D CMAKE_VERBOSE_MAKEFILE=YES ..
+    make check -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing Data-Sample
-mkdir -p lib/Data-Sample/build
-cd lib/Data-Sample/build
-../configure -C
-make -j $njobs
-sudo make install
-cd "$script_directory"
+if [ ! -d lib/Data-Sample/build ]; then
+    mkdir -p lib/Data-Sample/build
+    cd lib/Data-Sample/build
+    ../configure -C
+    make -j $njobs
+    sudo make install
+    cd "$script_directory"
+fi
 
 
 # Installing Data-miplib3
-mkdir -p lib/Data-miplib3/build
-cd lib/Data-miplib3/build
-../configure -C
-make -j $njobs
-sudo make install
-cd "$script_directory"
+if [ ! -d lib/Data-miplib3/build ]; then
+    mkdir -p lib/Data-miplib3/build
+    cd lib/Data-miplib3/build
+    ../configure -C
+    make -j $njobs
+    sudo make install
+    cd "$script_directory"
+fi
 
 
 # Installing Data-Netlib
-mkdir -p lib/Data-Netlib/build
-cd lib/Data-Netlib/build
-../configure -C
-make -j $njobs
-sudo make install
-cd "$script_directory"
+if [ ! -d lib/Data-Netlib/build ]; then
+    mkdir -p lib/Data-Netlib/build
+    cd lib/Data-Netlib/build
+    ../configure -C
+    make -j $njobs
+    sudo make install
+    cd "$script_directory"
+fi
 
 
 # Installing CoinUtils
-mkdir -p lib/CoinUtils/build
-cd lib/CoinUtils/build
-../configure -C
-make -j $njobs
-make test -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/CoinUtils/build ]; then
+    mkdir -p lib/CoinUtils/build
+    cd lib/CoinUtils/build
+    ../configure -C
+    make -j $njobs
+    make test -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installign OSI
-mkdir -p lib/Osi/build
-cd lib/Osi/build
-../configure -C
-make -j $njobs
-make test -j $njobs
-sudo make install 
-cd "${script_directory}"
+if [ ! -d lib/Osi/build ]; then
+    mkdir -p lib/Osi/build
+    cd lib/Osi/build
+    ../configure -C
+    make -j $njobs
+    make test -j $njobs
+    sudo make install 
+    cd "${script_directory}"
+fi
 
 
 # Installing CLP
-mkdir -p lib/Clp/build
-cd lib/Clp/build
-../configure -C
-make -j $njobs
-make test -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/Clp/build ]; then
+    mkdir -p lib/Clp/build
+    cd lib/Clp/build
+    ../configure -C
+    make -j $njobs
+    make test -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing CGL
-mkdir -p lib/Cgl/build
-cd lib/Cgl/build
-../configure -C
-make -j $njobs
-make test -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/Cgl/build ]; then
+    mkdir -p lib/Cgl/build
+    cd lib/Cgl/build
+    ../configure -C
+    make -j $njobs
+    make test -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 
 # Installing Cbc
-mkdir -p lib/Cbc/build
-cd lib/Cbc/build
-../configure -C
-make -j $njobs
-make test -j $njobs
-sudo make install
-cd "${script_directory}"
+if [ ! -d lib/Cbc/build ]; then
+    mkdir -p lib/Cbc/build
+    cd lib/Cbc/build
+    ../configure -C
+    make -j $njobs
+    make test -j $njobs
+    sudo make install
+    cd "${script_directory}"
+fi
 
 set +uexo
 
