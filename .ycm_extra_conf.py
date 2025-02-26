f0ed8a5dab908269531227cbfea23928ae82b0c1
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2025-02-26 18:45:20 -0500

    Updated makefiles and YCM configuration.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,16 +1,52 @@
+# This file is NOT licensed under the GPLv3, which is the license for the rest
+# of YouCompleteMe.
+#
+# Here's the license text for this file:
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+
+from sysconfig import get_path
+import platform
+import os.path as p
+import subprocess
 import os
-import ycm_core
-from clang_helpers import PrepareClangFlags
 
+DIR_OF_THIS_SCRIPT = p.abspath( p.dirname( __file__ ) )
+DIR_OF_THIRD_PARTY = p.join( DIR_OF_THIS_SCRIPT, 'third_party' )
+DIR_OF_WATCHDOG_DEPS = p.join( DIR_OF_THIRD_PARTY, 'watchdog_deps' )
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
 
-SEARCH_LIMIT = 21
+database = None
+
+SEARCH_LIMIT = 41
 
 cpp_include_template = '/usr/include/c++/{}'
 x86_64_include_template = '/usr/include/x86_64-linux-gnu/c++/{}'
-lib_x86_64_include_template = '/usr/lib/gcc/x86_64-linux-gnu/{}/include/'
 backward_include_template = '/usr/include/c++/{}/backward'
-llvm_template_with_dot = '/usr/lib/llvm-{}.{}/lib/clang/'
-llvm_template_without_dot = '/usr/lib/llvm-{}/lib/clang/'
+llvm_template = '/usr/lib/llvm-{}/lib/clang/'
 clang_template = '{}.{}.{}/include'
 
 def fill_line_once(line):
@@ -19,72 +55,84 @@ def fill_line_once(line):
         filled = line.format(version)
         if os.path.exists(filled):
             response = filled
-    if response is None:
-        resposne = ""
+    assert response is not None
     return response
 
-def fill_clang_line_with_dot(llvm_template, clang_template):
+def fill_clang_line(llvm_template, clang_template):
     response = None
-    for major in range(SEARCH_LIMIT):
-        for minor in range(SEARCH_LIMIT):
-            llvm_path = llvm_template.format(major, minor)
-            if os.path.exists(llvm_path):
-                for suffix in range(SEARCH_LIMIT):
-                    clang_path = llvm_path + clang_template.format(major, minor, suffix)
-                    if os.path.exists(clang_path):
-                        response = clang_path
-    return response
-
-def fill_clang_line_without_dot(llvm_template, clang_template):
-    response = None
-    for major in range(SEARCH_LIMIT):
-        llvm_path = llvm_template.format(major)
+    for version in range(SEARCH_LIMIT):
+        llvm_path = llvm_template.format(version)
         if os.path.exists(llvm_path):
+            major = version
             for minor in range(SEARCH_LIMIT):
                 for suffix in range(SEARCH_LIMIT):
                     clang_path = llvm_path + clang_template.format(major, minor, suffix)
                     if os.path.exists(clang_path):
                         response = clang_path
+    assert response is not None
     return response
-
 
 cpp_include_line = fill_line_once(cpp_include_template)
 x86_64_include_line = fill_line_once(x86_64_include_template)
-lib_x86_64_include_line = fill_line_once(lib_x86_64_include_template)
 backward_include_line = fill_line_once(backward_include_template)
-clang_include_line = fill_clang_line_with_dot(llvm_template_with_dot, clang_template)
-if clang_include_line is None:
-    clang_include_line = fill_clang_line_without_dot(llvm_template_without_dot, clang_template)
+clang_include_line = fill_clang_line(llvm_template, clang_template)
 
 
+# These are the compilation flags that will be used in case there's no
+# compilation database set (by default, one is not set).
+# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
+    '-std=c++20',
     '-Wall',
     '-Wextra',
-    '-std=c++20',
-    '-cc1',
-    '-triple',
     '-Woverflow',
     '-ftrapv',
+    '-Werror',
+    '-Wno-long-long',
+    '-Wno-variadic-macros',
+    '-Qunused-arguments',
+    '-fexceptions',
+    '-DNDEBUG',
     '-x',
     'c++',
-    '-pthread',
-    '-stdlib=libc++',
     '-isystem',
-    '/usr/local/include',
+    'cpp/absl',
+    '-isystem',
+    'cpp/pybind11',
+    '-isystem',
+    'cpp/whereami',
+    '-isystem',
+    'cpp/BoostParts',
+    '-isystem',
+    get_path( 'include' ),
+    '-isystem',
+    'cpp/llvm/include',
+    '-isystem',
+    'cpp/llvm/tools/clang/include',
+    '-I',
+    'cpp/ycm',
+    '-I',
+    'cpp/ycm/ClangCompleter',
+    '-isystem',
+    'cpp/ycm/tests/gmock/googlemock/include',
+    '-isystem',
+    'cpp/ycm/tests/gmock/googletest/include',
+    '-isystem',
+    'cpp/ycm/benchmarks/benchmark/include',
     '-isystem',
     cpp_include_line,
     '-isystem',
     x86_64_include_line,
     '-isystem',
-    lib_x86_64_include_line,
-    '-isystem',
     backward_include_line,
     '-isystem',
     clang_include_line,
-    '-isystem',
+     '-isystem',
     '/usr/include/x86_64-linux-gnu',
     '-isystem',
     '/usr/include',
+    '-isystem',
+    '/usr/local/include',
     '-isystem',
     '/usr/local/include/coin-or',
     '-isystem',
@@ -92,83 +140,76 @@ flags = [
     '-isystem',
 ]
 
-
+# Set this to the absolute path to the folder (NOT the file!) containing the
+# compile_commands.json file to use that instead of 'flags'. See here for
+# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
+#
+# You can get CMake to generate this file for you by adding:
+#   set( CMAKE_EXPORT_COMPILE_COMMANDS 1 )
+# to your CMakeLists.txt file.
+#
+# Most projects will NOT need to set this to anything; you can just change the
+# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
-
-if os.path.exists( compilation_database_folder ):
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
-else:
-  database = None
-
-SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm' ]
-
-def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
-
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return list( flags )
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
-
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
-
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
-
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
 
 
 def IsHeaderFile( filename ):
-  extension = os.path.splitext( filename )[ 1 ]
-  return extension in [ '.h', '.hxx', '.hpp', '.hh', '.cpp', '.c', '' ]
+  extension = p.splitext( filename )[ 1 ]
+  return extension in [ '.h', '.hxx', '.hpp', '.hh' ]
 
 
-def GetCompilationInfoForFile( filename ):
-  # The compilation_commands.json file generated by CMake does not have entries
-  # for header files. So we do our best by asking the db for flags for a
-  # corresponding source file, if any. If one exists, the flags for that file
-  # should be good enough.
+def FindCorrespondingSourceFile( filename ):
   if IsHeaderFile( filename ):
-    basename = os.path.splitext( filename )[ 0 ]
+    basename = p.splitext( filename )[ 0 ]
     for extension in SOURCE_EXTENSIONS:
       replacement_file = basename + extension
-      if os.path.exists( replacement_file ):
-        compilation_info = database.GetCompilationInfoForFile(
-          replacement_file )
-        if compilation_info.compiler_flags_:
-          return compilation_info
+      if p.exists( replacement_file ):
+        return replacement_file
+  return filename
+
+
+def PathToPythonUsedDuringBuild():
+  try:
+    filepath = p.join( DIR_OF_THIS_SCRIPT, 'PYTHON_USED_DURING_BUILDING' )
+    with open( filepath ) as f:
+      return f.read().strip()
+  except OSError:
     return None
-  return database.GetCompilationInfoForFile( filename )
 
 
-def FlagsForFile( filename, **kwargs ):
-  if database:
+def Settings( **kwargs ):
+  # Do NOT import ycm_core at module scope.
+  import ycm_core
+
+  global database
+  if database is None and p.exists( compilation_database_folder ):
+    database = ycm_core.CompilationDatabase( compilation_database_folder )
+
+  language = kwargs[ 'language' ]
+
+  if language == 'cfamily':
+    # If the file is a header, try to find the corresponding source file and
+    # retrieve its flags from the compilation database if using one. This is
+    # necessary since compilation databases don't have entries for header files.
+    # In addition, use this source file as the translation unit. This makes it
+    # possible to jump from a declaration in the header file to its definition
+    # in the corresponding source file.
+    filename = FindCorrespondingSourceFile( kwargs[ 'filename' ] )
+
+    if not database:
+      return {
+        'flags': flags,
+        'include_paths_relative_to_dir': DIR_OF_THIS_SCRIPT,
+        'override_filename': filename
+      }
+
+    compilation_info = database.GetCompilationInfoForFile( filename )
+    if not compilation_info.compiler_flags_:
+      return {}
+
     # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object
-    compilation_info = GetCompilationInfoForFile( filename )
-    if not compilation_info:
-      return None
-
-    final_flags = MakeRelativePathsInFlagsAbsolute(
-      compilation_info.compiler_flags_,
-      compilation_info.compiler_working_dir_ )
+    # python list, but a "list-like" StringVec object.
+    final_flags = list( compilation_info.compiler_flags_ )
 
     # NOTE: This is just for YouCompleteMe; it's highly likely that your project
     # does NOT need to remove the stdlib flag. DO NOT USE THIS IN YOUR
@@ -177,11 +218,54 @@ def FlagsForFile( filename, **kwargs ):
       final_flags.remove( '-stdlib=libc++' )
     except ValueError:
       pass
-  else:
-    relative_to = DirectoryOfThisScript()
-    final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
 
-  return {
-    'flags': final_flags,
-    'do_cache': True
-  }
+    return {
+      'flags': final_flags,
+      'include_paths_relative_to_dir': compilation_info.compiler_working_dir_,
+      'override_filename': filename
+    }
+
+  if language == 'python':
+    return {
+      'interpreter_path': PathToPythonUsedDuringBuild(),
+      'ls': {
+        'python': {
+          'analysis': {
+            'extraPaths': [
+              p.join( DIR_OF_THIS_SCRIPT ),
+              p.join( DIR_OF_THIRD_PARTY, 'bottle' ),
+              p.join( DIR_OF_THIRD_PARTY, 'regex-build' ),
+              p.join( DIR_OF_THIRD_PARTY, 'frozendict' ),
+              p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'jedi' ),
+              p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'parso' ),
+              p.join( DIR_OF_WATCHDOG_DEPS, 'watchdog', 'build', 'lib3' ),
+              p.join( DIR_OF_WATCHDOG_DEPS, 'pathtools' ),
+              p.join( DIR_OF_THIRD_PARTY, 'waitress' )
+            ],
+            'useLibraryCodeForTypes': True
+          }
+        }
+      }
+    }
+
+  return {}
+
+
+def PythonSysPath( **kwargs ):
+  sys_path = kwargs[ 'sys_path' ]
+
+  sys_path[ 0:0 ] = [ p.join( DIR_OF_THIS_SCRIPT ),
+                      p.join( DIR_OF_THIRD_PARTY, 'bottle' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'regex-build' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'frozendict' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'jedi' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'parso' ),
+                      p.join( DIR_OF_WATCHDOG_DEPS,
+                              'watchdog',
+                              'build',
+                              'lib3' ),
+                      p.join( DIR_OF_WATCHDOG_DEPS, 'pathtools' ),
+                      p.join( DIR_OF_THIRD_PARTY, 'waitress' ) ]
+
+  sys_path.append( p.join( DIR_OF_THIRD_PARTY, 'jedi_deps', 'numpydoc' ) )
+  return sys_path
