f518ea9768ce535dc3cbeb69a6311c4d01ef6ee4
Aliaksandr Nekrashevich <aliaksandr.nekrashevich@queensu.ca>
2025-03-11 12:11:44 -0400

    Reorganized compilers

--- compilers.sh
+++ compilers.sh
@@ -8,15 +8,19 @@
 
 export PATH="$PATH":/usr/local/gcc-14.2.0/bin:/usr/lib/llvm-19/bin
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":/usr/local/gcc-14.2.0/lib64:/usr/lib/llvm-19/lib
-export ASAN_OPTIONS=detect_leaks=0
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
+
+export CPATH="$CPATH:/usr/local/include/coin-or/glpk:/usr/local/include/coin-or"
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/local/include/coin-or/glpk"
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/local/include/coin-or "
+export C_INCLUDE_PATH="$C_INCLUDE_PATH:/usr/local/include/coin-or/glpk"
 
 c_compiler="gcc-14.2.0"
-cxx_compiler="g++-14.2.0"
-
 if ! $c_compiler --version; then
     c_compiler=gcc
 fi
 
+cxx_compiler="g++-14.2.0"
 if ! $cxx_compiler --version; then
     cxx_compiler=g++
 fi
@@ -28,8 +32,4 @@ export C="$c_compiler"
 export CC="$c_compiler"
 export CXX="$cxx_compiler"
 
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/lib"
-export CPATH="$CPATH:/usr/local/include/coin-or/glpk:/usr/local/include/coin-or"
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/local/include/coin-or/glpk"
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/local/include/coin-or "
-export C_INCLUDE_PATH="$C_INCLUDE_PATH:/usr/local/include/coin-or/glpk"
+export ASAN_OPTIONS=detect_leaks=0
